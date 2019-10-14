7410a68db6799902f72236af74ab2e0a19c5d72f
Stoica Tedy <stoicatedy@gmail.com>
2019-10-14 21:03:39 +0300

    sad

--- solar/solar.ino
+++ solar/solar.ino
@@ -17,8 +17,12 @@
 #define Y1_PIN 3
 #define Y2_PIN 4
 #define HYDRO_PIN 5
+#define STEPPER_PIN1 11
+#define STEPPER_PIN2 10
+#define STEPPER_PIN3 9
+#define STEPPER_PIN4 8
 
-Stepper stepper_x = Stepper(STEPS_PER_REVOLUTION, 11, 10, 9, 8);
+Stepper stepper_y = Stepper(STEPS_PER_REVOLUTION, STEPPER_PIN1, STEPPER_PIN2, STEPPER_PIN3, STEPPER_PIN4);
 int last_direction = -1;
 int hydro_timeout = 0;
 
@@ -27,7 +31,7 @@ int hydro_timeout = 0;
 */
 unsigned int stepper_position;
 void setup() {
-    stepper_x.setSpeed(STEPPER_SPEED);
+    stepper_y.setSpeed(STEPPER_SPEED);
     Serial.begin(9600);
     stepper_position = EEPROM.read(0);
 
@@ -78,7 +82,7 @@ int direction() {
     return id + 1;
 }
 
-void move_to_x(int direction) {
+void move_to_y(int direction) {
     int angle_needed = 0;
     // 22.5 degrees per direction
     if(direction * 22.5 + stepper_position > 180) {
@@ -97,7 +101,11 @@ void move_to_x(int direction) {
     Serial.print(angle_needed);
     Serial.print('\n');
     // roughly 11.333 steps per degree
-    stepper_x.step(ceil(angle_needed * 11.333));
+    stepper_y.step(ceil(angle_needed * 11.333));
+}
+
+void move_to_x(int direction) {
+
 }
 
 void solar_axis_x(int new_direction = direction()) {
